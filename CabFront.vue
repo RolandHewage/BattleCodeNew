50c767999ab6bec8b8a78e619671136fbf404081
RolandHewage <roland.hewage@gmail.com>
2019-08-17 11:48:49 +0530

    MyCommit1

---
+++ CabFront.vue
@@ -0,0 +1,272 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      v-model="drawer"
+      stateless
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout
+            v-if="item.heading"
+            :key="item.heading"
+            row
+            align-center
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group
+            v-else-if="item.children"
+            v-model="item.model"
+            :key="item.text"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+              
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else :key="item.text" >
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="primary"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">GAP Maze CAB Profile</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        prepend-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>notifications</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" tile>
+          <img
+            src="https://vuetifyjs.com/static/doc-images/logo.svg"
+            alt="Vuetify"
+          >
+        </v-avatar>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout column >
+
+
+
+
+
+
+
+
+<v-card>
+        <v-container
+          fluid
+          grid-list-md
+        >
+          <v-layout row wrap>
+            <v-flex
+              v-for="card in cards"
+              v-bind="{ [`xs${card.flex}`]: true }"
+              :key="card.title"
+            >
+              <v-card :to="card.link">
+                <v-card-media
+                  :src="card.src"
+                  height="200px"
+                >
+                  <v-container
+                    fill-height
+                    fluid
+                    pa-2
+                  >
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.title"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-media>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>share</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+
+
+
+
+
+
+
+
+           
+
+        </v-layout>
+
+
+
+      </v-container>
+    </v-content>  
+            
+  </v-app>
+
+   
+</template>
+
+
+<script>
+export default {
+    data: () => ({
+      dialog: false,
+      drawer: null,
+      items: [
+        { icon: 'contacts', text: 'Contacts' },
+        { icon: 'history', text: 'Frequently contacted' },
+        { icon: 'content_copy', text: 'Duplicates' },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Labels',
+          model: true,
+          children: [
+            { icon: 'add', text: 'Create label' }
+          ]
+        },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'More',
+          model: false,
+          children: [
+            { text: 'Import' },
+            { text: 'Export' },
+            { text: 'Print' },
+            { text: 'Undo changes' },
+            { text: 'Other contacts' }
+          ]
+        },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Send feedback' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Go to the old version' }
+      ],
+
+      // Farmers List start
+      items1: [
+                { icon: true, title: 'Jason Oner', avatar: '/static/doc-images/lists/1.jpg' },
+                { title: 'Travis Howard', avatar: '/static/doc-images/lists/2.jpg' },
+                { title: 'Ali Connors', avatar: '/static/doc-images/lists/3.jpg' },
+                { title: 'Cindy Baker', avatar: '/static/doc-images/lists/4.jpg' }
+              ],
+      // Farmers List end
+
+        cards: [
+                    { title: 'Requests', src: '/static/doc-images/front/1.jpg', flex: 4, link: "/cabhome/requests" },
+                    { title: 'Audit', src: '/static/doc-images/front/2.jpg', flex: 4, link: "/cabhome/audit" },
+                    { title: 'Farmers', src: '/static/doc-images/front/3.jpg', flex: 4, link: "/cabhome/farmers" },
+                    { title: 'Crops', src: '/static/doc-images/front/4.jpg', flex: 4, link: "/cabhome/crops" },
+                    { title: 'Chemical Recomendations', src: '/static/doc-images/front/5.jpg', flex: 4, link: "/cabhome/crops" },
+                    { title: 'Fertilizer Recommendations', src: '/static/doc-images/front/6.jpg', flex: 4, link: "/cabhome/crops" }
+        ],
+
+
+
+
+
+       e1: 'recent',
+
+      //<!-- Bottom Navigation ..... start -->
+      e2: 3
+      //<!-- Bottom Navigation ..... end -->
+    }),
+    props: {
+      source: String
+    },
+
+    //<!-- Bottom Navigation ..... start -->
+    computed: {
+      color () {
+        switch (this.e2) {
+          case 0: return 'blue-grey'
+          case 1: return 'teal'
+          case 2: return 'brown'
+          case 3: return 'indigo'
+        }
+      }
+    }
+    //<!-- Bottom Navigation ..... end -->
+
+
+  }    
+</script>
+
+
+<style>
+
+</style>
